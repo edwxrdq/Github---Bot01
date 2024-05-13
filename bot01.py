deac12c0ef11b1c12f13480e16eda4ff7eff4a09
edwxrdq <edwardrquesada@gmail.com>
2024-05-12 23:37:06 -0700

    project set up, pip installs

--- bot01.py
+++ bot01.py
@@ -17,39 +17,39 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-client = commands.Bot(command_prefix = '!', intents=discord.Intents.all()) 
+bot = commands.Bot(command_prefix = '!', intents=discord.Intents.all()) 
 
 
 
-@client.event 
+@bot.event 
 async def on_ready(): 
     print("The bot is now ready for use!")
     print("-----------------------------")
 
-@client.command()
+@bot.command()
 async def hello(ctx):
     await ctx.send("hello, i am the first bot.")
 
-@client.command()
+@bot.command()
 async def goodbye(ctx):
     await ctx.send("goodbye, have a great rest of the day!")
 
 #detects when user joins server, then runs the following
-@client.event
+@bot.event
 async def on_member_join(member):
     await channel.send("someone has joined the server!")
     # await channel.send(f"welcome to the server {member.mention}")
-    channel = client.get_channel(1218445506049478659)
+    channel = bot.get_channel(1218445506049478659)
 
 
 #detects when user leaves server, then runs the following
-@client.event
+@bot.event
 async def on_member_remove(member):
-    channel = client.get_channel(1218445506049478659)
+    channel = bot.get_channel(1218445506049478659)
     await channel.send("someone has left the server!")
     # await channel.send(f'{member.mention} has left the server!')
 
-@client.command(pass_context = True)
+@bot.command(pass_context = True)
 async def join(ctx):
     #relies on user input
     if (ctx.author.voice):
@@ -59,7 +59,7 @@ async def join(ctx):
         await ctx.send("you must be in a voice channel to run this command.")
 
 
-@client.command(pass_context = True)
+@bot.command(pass_context = True)
 async def leave(ctx):
     #if the bot is in a voice channel, it'll run the following command; disconnects then sends message in chat
     if (ctx.voice_client):
@@ -70,14 +70,13 @@ async def leave(ctx):
         #if bot isn't in a voice channel then it'll run this
         await ctx.send("i'm not in a voice channel.")
 
-@client.event 
+@bot.listen('on_message')
 async def on_message(message):
     if message.content.casefold() == "nigga":
         await message.delete()
         await message.channel.send("that language is not allowed in this server!")
-        await client.process_commands(message)
 
-@client.command()
+@bot.command()
 @has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
@@ -88,16 +87,16 @@ async def kick_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("you don't have permission to kick people.")
 
-# @client.command()
-# @has_permissions(ban_members=True)
-# async def ban(ctx, member: discord.Member, *, reason=None):
-#     await member.ban(reason=reason)
-#     await ctx.send(f'user {member} has been banned.')
+@bot.command()
+@has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'user {member} has been banned.')
 
-# @ban.error
-# async def ban_error(ctx, error):
-#     if isinstance(error, commands.MissingPermissions):
-#         await ctx.send("you don't have permission to ban people.")
+@ban.error
+async def ban_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("you don't have permission to ban people.")
 
 
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
