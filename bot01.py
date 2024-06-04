da888eacb3cf9eaba485211c012ec37e21d1b100
edwxrdq <edwardrquesada@gmail.com>
2024-06-03 22:04:37 -0700

    attempt to add play, resume etc rythym functions unable to work, seems i cant get cog to work

--- bot01.py
+++ bot01.py
@@ -1,3 +1,4 @@
+import music as music
 import discord
 from discord.ext import commands 
 from discord import FFmpegPCMAudio
@@ -12,15 +13,70 @@ from discord.utils import get
 import requests
 import json
 # import os
+import youtube_dl
+import nacl
 
+bot = commands.Bot(command_prefix = '!', intents=discord.Intents.all()) 
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = '!', intents=discord.Intents.all()) 
+# def setup(bot):
+#      bot.add_cog(music(bot))
 
+cogs = [music]
+for i in range(len(cogs)):
+    cogs[i].setup(bot)
 
+# 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+# # class music(commands.Cog):
+# #     def __init__(self, bot):
+# #         self.client = bot
+
+# # def setup(client):
+# #     client.add_clog(music(bot))
+
+# # @bot.command()
+# # async def join(ctx):
+# #     if ctx.author.voice is None:
+# #         await ctx.send("you're not in a voice channel!")
+# #         voice_channel = ctx.message.author.voice.channel
+# #     if ctx.voice_client is None:
+# #         await voice_channel.connect()
+# #     else:
+# #         await ctx.voice_client.move_to(voice_channel)
+
+# @bot.command()
+# async def disconnect(ctx):
+#     await ctx.voice_client.disconnect()  
+
+# @bot.command()
+# async def play (ctx, url):
+#     ctx.voice_client.stop()
+#     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+#     YDL_OPTIONS = {'format':"bestaudio"}
+#     vc = ctx.voice_bot
+
+#     with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+#         info = ydl.extract_info(url, download=False)
+#         url2 = info['formats'][0]['url']
+#         source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+#         vc.play(source)
+
+# @bot.command()
+# async def pause(ctx):
+#     await ctx.voice_client.pause()
+#     await ctx.send('paused.')
+
+# @bot.command()
+# async def resume(ctx):
+#     await ctx.voice_client.resume()
+#     await ctx.send('resumed.')
+
+      
+# 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 
 @bot.event 
 async def on_ready(): 
@@ -175,5 +231,8 @@ async def removeRole(ctx, user : discord.Member, *, role : discord.Role):
 async def removeRole_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("you don't have permissions to add roles.")
+
+
+# bot.load_extension(f"cogs.{music}")
 
 bot.run(DISCORD_TOKEN)
